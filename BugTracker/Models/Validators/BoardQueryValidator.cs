0cc1932dd0671a654f05390c0293c5cdeff5c699
Mateusz <matehan97@gmail.com>
2022-12-30 14:24:36 +0100

    query validator for board query

---
+++ BugTracker/Models/Validators/BoardQueryValidator.cs
@@ -0,0 +1,22 @@
+﻿using BugTracker.Models.QueryModels;
+using FluentValidation;
+
+namespace BugTracker.Models.Validators
+{
+    public class BoardQueryValidator : AbstractValidator<BoardQuery>
+    {
+        private int[] allowedPagedSizes = new[] { 5, 10, 15, 30 };
+
+        public BoardQueryValidator()
+        {
+            RuleFor(b => b.PageNumber).GreaterThanOrEqualTo(1);
+            RuleFor(b => b.PageSize).Custom((value, context) =>
+            {
+                if (!allowedPagedSizes.Contains(value))
+                {
+                    context.AddFailure("PageSize", $"PageSize must be in [{string.Join(",", allowedPagedSizes)}]");
+                }
+            });
+        }
+    }
+}
